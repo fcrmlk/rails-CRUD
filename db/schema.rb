044f7cd0610bd9946c5752074ccc50734bb82799
Haider Awan <Haid3rAwan@icloud.com>
2019-07-24 05:22:59 +0500

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_220826) do
+ActiveRecord::Schema.define(version: 2019_07_22_163409) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.integer "postal"
     t.string "city"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "name"
+    t.string "price"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
